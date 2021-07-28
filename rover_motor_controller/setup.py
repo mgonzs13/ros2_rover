ce265a371e328c4200c2460b6a8872c63775ad6d
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-07-28 17:26:18 +0200

    fic

--- rover_motor_controller/setup.py
+++ rover_motor_controller/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share/launch/', package_name), glob('launch/*_launch.py')),
+        (os.path.join('share/', package_name, 'launch/'), glob('launch/*_launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
