9294c43bf4093d6f2e18d102a866795f86045b59
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-07-28 19:08:07 +0200

    fix

--- rover/launch/rover_launch.py
+++ rover/launch/rover_launch.py
@@ -9,7 +9,7 @@ from launch_ros.actions import PushRosNamespace
 
 def generate_launch_description():
     rover_shared_dir = get_package_share_directory(
-        "teleop_twist_joy")
+        "rover")
     teleop_twist_joy_shared_dir = get_package_share_directory(
         "teleop_twist_joy")
     rover_motor_controller_shared_dir = get_package_share_directory(
