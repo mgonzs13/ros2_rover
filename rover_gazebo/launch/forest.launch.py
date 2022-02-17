29ae76076ea8a6865e60d56aef48a56e25230a8c
Miguel Ángel González Santamarta <mgons@unileon.es>
2022-02-17 17:44:51 +0100

    forest world created

---
+++ rover_gazebo/launch/forest.launch.py
@@ -0,0 +1,28 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+
+    pkg_path = get_package_share_directory("rover_gazebo")
+
+    gazebo_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_path, "launch", "gazebo.launch.py")
+        ),
+        launch_arguments={
+            "world": os.path.join(pkg_path, "worlds", "forest.world"),
+            "initial_pose_x": "-3.0",
+            "initial_pose_y": "1.4",
+            "initial_pose_z": "4.2"
+        }.items()
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(gazebo_cmd)
+
+    return ld
