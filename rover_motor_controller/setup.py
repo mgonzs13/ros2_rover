b0deb9dc45121b7cd1554a7c59dc2f6e809ab2bf
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-01-10 12:05:31 +0100

    new version 1.1.1

--- rover_motor_controller/setup.py
+++ rover_motor_controller/setup.py
@@ -6,7 +6,7 @@ package_name = "rover_motor_controller"
 
 setup(
     name=package_name,
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
--- rover_teleop/setup.py
+++ rover_teleop/setup.py
@@ -6,7 +6,7 @@ package_name = "rover_teleop"
 
 setup(
     name=package_name,
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
