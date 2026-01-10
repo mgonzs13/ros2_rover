dbfcc40fdc721b52c3ed1e7f378f21c7859ea45b
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-01-10 12:32:32 +0100

    new version 2.0.0

--- rover_teleop/setup.py
+++ rover_teleop/setup.py
@@ -6,7 +6,7 @@ package_name = "rover_teleop"
 
 setup(
     name=package_name,
-    version="1.1.0",
+    version="2.0.0",
     packages=find_packages(),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
